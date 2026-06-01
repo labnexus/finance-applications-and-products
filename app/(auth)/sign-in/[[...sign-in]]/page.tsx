6216c33cba921e7a74dc8709131b9ae2b27d661b
adnane <elaboudi.adnane@gmail.com>
2026-06-01 13:31:59 +0100

    JVZoo

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
           </ClerkLoading>
         </div>
       </div>
-      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+      <div className="h-full bg-orange-600 hidden lg:flex items-center justify-center">
         <Image src="/logo.svg" height={100} width={100} alt="Logo" />
       </div>
     </div>
---
+++ app/jvzoo/page.tsx
@@ -0,0 +1,23 @@
+
+
+export default function JVZooPage() {
+  return (
+    <div>
+        <h1 className="border border-black bg-orange-600 w-full h-[200px] font-semibold text-white text-2xl ml-2.5 flex justify-center items-center">
+            Finance Applications Management - Sales Page
+        </h1>
+        <div className="w-full flex justify-center items-center justify-around">
+            <a href="https://www.jvzoo.com/b/0/442271/1" className="border border-black">
+                <img src="https://i.jvzoo.com/0/442271/1" alt="Finance Applications Management"/>
+            </a>
+            <a href="https://www.jvzoo.com/nothanks/442271" className="border border-black rounded-[50px] h-[100px] w-[200px] bg-orange-600 flex justify-center items-center content-between text-white text-2xl ml-2.5">
+                No Thanks
+            </a>
+        </div>
+        <div className="bg-red-600 border border-black rounded-[5px] w-[50%] m-[1%] p-[1%]">
+            Disclaimer: Please note that this product does not provide any guarantee of income or success. The results achieved by the product owner or any other individuals mentioned are not indicative of future success or earnings. This website is not affiliated with FaceBook or any of its associated entities. Once you navigate away from FaceBook, the responsibility for the content and its usage lies solely with the user. All content on this website, including but not limited to text, images, and multimedia, is protected by copyright law and the Digital Millennium Copyright Act. Unauthorized copying, duplication, modification, or theft, whether intentional or unintentional, is strictly prohibited. Violators will be prosecuted to the fullest extent of the law.
+            We want to clarify that JVZoo serves as the retailer for the products featured on this site. JVZoo® is a registered trademark of BBC Systems Inc., a Florida corporation located at 1809 E. Broadway Street, Suite 125, Oviedo, FL 32765, USA, and is used with permission. The role of JVZoo as a retailer does not constitute an endorsement, approval, or review of these products or any claims, statements, or opinions used in their promotion.
+        </div>
+    </div>
+  );
+};
